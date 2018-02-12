e7a23c96a8781a86ef1c6fb38598c0413edefff2
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-12 13:41:12 -0600

    created value template

--- src/app/Value/Value.component.ts
+++ src/app/Value/Value.component.ts
@@ -16,8 +16,8 @@ export class ValueComponent implements OnInit {
   }
 
   getValues() {
-    this.http.get('http://localhost:5000/api/value').subscribe(response => {
-      console.log(response);
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response.json();
     });
   }
 
